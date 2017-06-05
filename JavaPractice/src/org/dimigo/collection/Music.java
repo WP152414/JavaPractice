f6de8679cbb6aa75810c01a1ccd251fbdbd4b695
안문수 <@DESKTOP-K0S8EB9>
2017-06-05 10:04:34 +0900

    실습 과제 15

---
+++ JavaPractice/src/org/dimigo/collection/Music.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package org.dimigo.collection;
+
+/**
+ * <pre>
+ * org.dimigo.basic
+ *   |_ HelloWorld
+ *
+ * 1. 개요 :
+ * 2. 작성일 : 3/6
+ * </pre>
+ *
+ * @author      : 안문수
+ * @version     : 1.0
+ */
+public class Music {
+
+	private String title;
+	private String singer;
+	
+	public Music(String title, String singer) {
+		this.title = title;
+		this.singer = singer;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getSinger() {
+		return singer;
+	}
+
+	public void setSinger(String singer) {
+		this.singer = singer;
+	}
+
+	@Override
+	public String toString() {
+		return title +" ("+ singer + ")";
+	}
+	
+	
+	
+}
